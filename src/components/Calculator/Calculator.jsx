e61b40f0c3b69f8c54be6e378fb779912314f60d
Ayo Osho <aosho44@gmail.com>
2022-08-19 06:10:46 +0100

    Attempt proptype and useState

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,44 +1,108 @@
 import './Calculator.css';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Calculator() {
+	const [number, setNumber] = useState('');
+
+	const changeHandler = (event) => {
+		setNumber(event.target.value);
+	};
+
+	const clickHandler = (event) => {
+		setNumber(event.target.value);
+	};
+
 	return (
 		<div id="container">
 			<div id="calculatorBody">
 				<div>
-					<input type="number" id="calcScreen" />
+					<input
+						type="number"
+						id="calcScreen"
+						onChange={changeHandler}
+						value={number}
+					/>
 				</div>
 				<div id="calculatorBottom">
 					<div id="functionDiv">
-						<button className="funcButton">+</button>
-						<button className="funcButton">-</button>
-						<button className="funcButton">x</button>
-						<button className="funcButton">÷</button>
+						<button className="funcButton" onClick={clickHandler}>
+							+
+						</button>
+						<button className="funcButton" onClick={clickHandler}>
+							-
+						</button>
+						<button className="funcButton" onClick={clickHandler}>
+							x
+						</button>
+						<button className="funcButton" onClick={clickHandler}>
+							÷
+						</button>
 					</div>
 					<div id="numDiv">
 						<div id="numSplitDiv">
-							<button className="numberButton">7</button>
-							<button className="numberButton">4</button>
-							<button className="numberButton">1</button>
-							<button className="numberButton">0</button>
+							<button className="numberButton" value={7} onClick={clickHandler}>
+								7
+							</button>
+							<button className="numberButton" value={4} onClick={clickHandler}>
+								4
+							</button>
+							<button className="numberButton" value={1} onClick={clickHandler}>
+								1
+							</button>
+							<button className="numberButton" value={0} onClick={clickHandler}>
+								0
+							</button>
 						</div>
 						<div id="numSplitDiv">
-							<button className="numberButton">8</button>
-							<button className="numberButton">5</button>
-							<button className="numberButton">2</button>
-							<button className="numberButton">.</button>
+							<button className="numberButton" value={8} onClick={clickHandler}>
+								8
+							</button>
+							<button className="numberButton" value={5} onClick={clickHandler}>
+								5
+							</button>
+							<button className="numberButton" value={2} onClick={clickHandler}>
+								2
+							</button>
+							<button
+								className="numberButton"
+								value={'.'}
+								onClick={clickHandler}
+							>
+								.
+							</button>
 						</div>
 						<div id="numSplitDiv">
-							<button className="numberButton">9</button>
-							<button className="numberButton">6</button>
-							<button className="numberButton">3</button>
-							<button className="numberButton">AC</button>
+							<button className="numberButton" value={9} onClick={clickHandler}>
+								9
+							</button>
+							<button className="numberButton" value={6} onClick={clickHandler}>
+								6
+							</button>
+							<button className="numberButton" value={3} onClick={clickHandler}>
+								3
+							</button>
+							<button
+								className="numberButton"
+								value={''}
+								onClick={clickHandler}
+							>
+								AC
+							</button>
 						</div>
-						<button className="equalsButton">=</button>
+						<button className="equalsButton" onClick={clickHandler}>
+							=
+						</button>
 					</div>
 				</div>
 			</div>
 		</div>
 	);
 }
+
+Calculator.prototype = {
+	clickHandler: PropTypes.number.isRequired,
+	changeHandler: PropTypes.number.isRequired,
+};
 
 export { Calculator };
